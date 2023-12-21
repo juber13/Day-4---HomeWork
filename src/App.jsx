a06f2a147fed3ce4f6a17d83ffe4433881b842a9
juber <juberk1010@gmail.com>
2023-12-21 22:34:59 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import Header from "./components/Header"
-import Home from "./pages/Home"
 import Quote from "./pages/Quote"
 import Footer from "./components/Footer"
-import {Routes , Route} from 'react-router-dom'
+// import {Routes , Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <>
     <Header/>
-     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/quote" element={<Quote/>} />
-     </Routes>
+     <Quote/>
     <Footer/>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"
 const Header = () => {
     return (
         <header className="w-full bg-black text-white p-5 items-center sticky z-10 top-0">
@@ -11,11 +10,11 @@ const Header = () => {
             </div>
             <div className="center">
                 <ul className="flex gap-6 font-medium">
-                    <li className="text-blue-500"><Link to="/">Home</Link></li>
-                    <li><Link to="/quote">Quote</Link></li>
-                    <li><Link to="/Restaurents">Restaurents</Link></li>
-                    <li><Link to="/Foods">Foods</Link></li>
-                    <li><Link to="/Contact">Contact</Link></li>
+                    <li className="text-blue-500"><a href="#">Home</a></li>
+                    <li><a href="#">Quote</a></li>
+                    <li><a href="#">Restaurents</a></li>
+                    <li><a href="#">Foods</a></li>
+                    <li><a href="#">Contact</a></li>
                 </ul>
             </div>
             <div className="right">
